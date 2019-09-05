d3ff94e30ab921c854ced2a80ceed0490d3ca528
Simon Jansson <simonjansson92@gmail.com>
2019-09-05 14:55:52 +0200

    add comments to script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+//Sun vs Moon
+
+//Variables
 const sun = document.getElementById("image-sun-moon");
 var isSun = true;
 
+//When image is clicked change to moon if sun is showing, and sun if moon is showing.
 sun.onclick = function(){
         if(isSun){
             sun.src = "moon.jpg";
